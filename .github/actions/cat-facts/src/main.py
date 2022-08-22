8e1cd6ebe4888731810e29dea4657ef4ed2e14d0
theomeka <98803393+theomeka@users.noreply.github.com>
2022-08-22 13:49:48 +0700

    Update main.py

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -3,8 +3,22 @@ import random
 import sys
 
 # Make an HTTP GET request to the cat-fact API
-cat_url = "https://cat-fact.herokuapp.com/facts"
-r = requests.get(cat_url)
+# cat_url = "https://cat-fact.herokuapp.com/facts"
+# r = requests.get(cat_url)
+r = '''{
+  "_id": "58e008ad0aac31001185ed0c",
+  "text": "The frequency of a domestic cat;s purr is the same at which muscles and bones repair themselves.",
+  "type": "cat",
+  "user": {
+    "_id": "58e007480aac31001185ecef",
+    "name": {
+      "first": "Kasimir",
+      "last": "Schulz"
+    }
+  },
+  "upvotes": 6,
+  "userUpvoted": "None"
+}'''
 r_obj_list = r.json()
 
 # Create an empty list to store individual facts in
